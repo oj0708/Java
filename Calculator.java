1db1b213712fc80f5b3f9d22a997a966a5bc4e9e
oj0708 <oj0708@gmail.com>
2018-09-21 10:45:20 -0400

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+		public void calculator() {}
+			public double add(double operator1, double operator2 ) {
+						return operator1+operator2;
+							}
+				public double subtract(double operator1, double operator2 ) {
+							return operator1-operator2;
+								}
+					public double multiply(double operator1, double operator2 ) {
+								return operator1*operator2;
+									}
+						public double divid(double operator1, double operator2 ) {
+									return operator1/operator2;
+										}
+							public double modulus(double operator1, double operator2 ) {
+										return operator1%operator2;
+											}
+
+
+}
+
